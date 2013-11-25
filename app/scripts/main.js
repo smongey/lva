a2cba9962827bfb3e81c116cd9c31374e85a289b
Sean Mongey <sm@smongey.com>
2013-11-25 16:48:47 +0000

    updated member button, fixed home banner to background image and fixed logo and tagline placement

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,59 +7,30 @@ $(document).ready(function(){
 
 	memberButton();
 
-	if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
-
-		$('#slides .title').css({
-			'margin-top' : -140
-		});	
-
-	} else {
-
-		var titleHeight = $('.title').height()/2;
-		var slideHeight = $('.holder').height()/2;
-		$('#slides .title').css({
-			'margin-top' : -(slideHeight + titleHeight - 20)
-		});		
-
-	}
-
-
 	$(window).resize(function(){
-		titlePosition();
 		memberButton();
 	});
 
 
 });
 
-titlePosition = function(){
+memberButton = function(){
 
-	if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+	if ($(window).width() > 1200) {
 
-		$('#slides .title').css({
-			'margin-top' : -140
-		});	
+		$('li.member a').hover(function(){
+			$('li.membertext a').addClass('active');
+		}, function(){
+			$('li.membertext a').removeClass('active');
+		});
 
 	} else {
 
-		var titleHeight = $('.title').height()/2;
-		var slideHeight = $('.holder').height()/2;
-		$('#slides .title').css({
-			'margin-top' : -(slideHeight + titleHeight - 20)
-		});		
-
-	}
-
-	console.log( -(slideHeight + titleHeight - 20) );
-};
-
-
-memberButton = function(){
-	if ($(window).width() > 1200) {
 		$('li.member a').hover(function(){
-			$('li.membertext a').animate({'margin-right':'0px'}, 150);
+			$('li.membertext a').removeClass('active');
 		}, function(){
-			$('li.membertext a').animate({'margin-right':'-160px'}, 150);
+			$('li.membertext a').removeClass('active');
 		});
+
 	}
 };
